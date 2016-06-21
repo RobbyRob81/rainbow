6701b1bd0c6d0d53d0c07b234b592faf01342ce7
Rob Rosario <robbysrosario@gmail.com>
2016-06-21 14:57:30 -0700

    css and html finished.  time to quickly add some functionality!!

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
   render() {
-    return <div>Hello world</div>
+    return (
+      <div>
+        <div id='rainbowContainer'>
+            <div id="rainbow">
+              <div className="row">
+               <div className="rowB">
+                 <div className="rowC">
+                   <div className="insideRow"></div>
+                 </div>
+               </div>
+              </div>
+            </div>
+            <div id="rainbow-fill"></div>
+            <div id='cloud' className="cloudLeft"></div>
+            <div id='cloud' className="cloudRight"></div>
+          </div>
+        </div>
+      )
   }
 }
 
